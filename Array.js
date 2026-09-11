f4028661f77b3db4b8220794d1aa5fcf46e9766f
jpedinacio03 <joao.inacio03@escola.pr.gov.br>
2026-09-11 11:33:59 -0300

    Add Array.js with fruit and number examples

---
+++ Array.js
@@ -0,0 +1,12 @@
+let fruta = "maça"; 
+//tipo -  nome da variavel - numero de posições 
+let frutas = ["maça", "pera", "abacate"];
+console.log(fruta);
+console.log(frutas);
+
+let numeros = [];
+numeros.push(4);
+numeros.push("melancia");
+
+console.log(numeros);
+console.log(frutas[1]);
